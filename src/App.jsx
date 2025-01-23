531577f246b7192cca730f479f461c6bd523f151
lalalala55 <gopisai4568@gmail.com>
2025-01-23 17:37:49 +0530

    356. Time to Practice: Problem - Begining Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import RootLayout from './pages/RootLayout';
-import HomePage from './pages/HomePage';
-import ProductsPage from './pages/ProductsPage';
-import ProductPage from './pages/ProductPage';
-import ErrorPage from './pages/ErrorPage';
+// Challenge / Exercise
 
-const router = createBrowserRouter([
-  {
-    path: '/', 
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { 
-        index: true,
-        path: '/', 
-        element: <HomePage />
-      },
-      { 
-        path: 'products', 
-        element: <ProductsPage />,
-      },
-      {
-        path: 'products/:id',
-        element: <ProductPage />
-      }
-    ]
-  }
-])
+// 1. Add five new (dummy) page components (content can be simple <h1> elements)
+//    - HomePage
+//    - EventsPage
+//    - EventDetailPage
+//    - NewEventPage
+//    - EditEventPage
+// 2. Add routing & route definitions for these five pages
+//    - / => HomePage
+//    - /events => EventsPage
+//    - /events/<some-id> => EventDetailPage
+//    - /events/new => NewEventPage
+//    - /events/<some-id>/edit => EditEventPage
+// 3. Add a root layout that adds the <MainNavigation> component above all page components
+// 4. Add properly working links to the MainNavigation
+// 5. Ensure that the links in MainNavigation receive an "active" class when active
+// 6. Output a list of dummy events to the EventsPage
+//    Every list item should include a link to the respective EventDetailPage
+// 7. Output the ID of the selected event on the EventDetailPage
+// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 function App() {
-  return (
-      <RouterProvider router={router} />
-  );
+  return <div>
+    <h1>Hellow</h1>
+  </div>;
 }
 
 export default App;
