aeb0868c08eb6dc842ef397eac4396a1cf7f6a42
lalalala55 <gopisai4568@gmail.com>
2025-01-27 11:38:59 +0530

    364. Reflecting The Current Navigation State in the UI

--- src/components/EventsLayout.jsx
+++ src/components/EventsLayout.jsx
@@ -1,8 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "./MainNavigation";
 
 function EventsLayout() {
+    const navigation = useNavigation();
+
     return <>
+        {navigation.state === 'loading' && <h1>Loading....</h1>}
         <MainNavigation />
         <Outlet />
     </>
